0c7962788848ab8138e6059c09a99a5412115f11
wakenedo <a_nuner@outlook.com>
2022-04-11 10:25:25 -0300

    useCallback
    The only usage of useCallback is for referential equality.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useCallback, useState } from 'react'
 import { SearchResults } from '../components/SearchResults'
 
 
@@ -19,6 +19,10 @@ export default function  Home()  {
     setResults(data)
   }
 
+  const addToWishList = useCallback(async (id: number) => {
+    console.log(id)
+  }, [])
+
   return (
     <div >
       <h1>Search</h1>
@@ -31,7 +35,9 @@ export default function  Home()  {
         <button type='submit'>Search</button>
       </form>
 
-      <SearchResults results={results}/>
+      <SearchResults 
+      results={results}
+      onAddToWishList={addToWishList}/>
     </div>
   )
 }
